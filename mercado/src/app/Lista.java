3c4ed5b218ae6f7fed051aeab04ba9555105adb5
Daniel Barreto <danielbarreto559@gmail.com>
2020-03-31 11:01:45 -0300

    Insert at start and at last

--- mercado/src/app/Lista.java
+++ mercado/src/app/Lista.java
@@ -15,4 +15,61 @@ public class Lista {
         this.ult = null;
         this.qtd = 0;
     }
+
+    public No getPrim() {
+        return prim;
+    }
+
+    public void setPrim(No prim) {
+        this.prim = prim;
+    }
+
+    public No getUlt() {
+        return ult;
+    }
+
+    public void setUlt(No ult) {
+        this.ult = ult;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }//----------------------------------------------GETS AND SETTERS
+
+    public void insertFirst(Produto item)
+    {
+        No novoNO = new No(item);
+        if (this.isEmpity()) {
+            this.ult = novoNO;
+        }
+        novoNO.setProx(this.prim);
+        this.prim = novoNO;
+        this.qtd++;
+
+    }//--------------------------------------------------------INSERT AT START NODE
+
+    public boolean isEmpity(){return(this.prim == null);}//----IS EMPTY
+
+    public void insertLast(Produto item) {
+        No novoNo = No(item);
+        if (this.isEmpity()) {
+            this.prim = novoNo;
+        } else {
+            this.ult.setProx(novoNo);
+        }
+        this.ult = novoNo;
+        this.qtd++;
+    }//--------------------------------------------------------INSERT AT LAST POSITION
+    
+
+
+    
+
+
+
+
 }
